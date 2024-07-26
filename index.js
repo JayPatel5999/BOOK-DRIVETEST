c66a537989a0217ff4d16a2704b64325fc00cfed
JayPatel5999 <Jpatel9102@conestogac.on.ca>
2024-07-25 21:33:35 -0400

    changes

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const app = new express()
-const path = require('path')
-const ejs = require('ejs')
-app.set('view engine','ejs')
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
 const mongoose = require('mongoose');
-const User = require('./models/User');
 const session = require('express-session');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const flash = require('req-flash');
 
+// Set view engine to ejs
+app.set('view engine', 'ejs');
 
-mongoose.set('strictQuery', false); 
+// Set Mongoose strictQuery option
+mongoose.set('strictQuery', false);
 
 // Connect to MongoDB using environment variable
 const mongoURI = process.env.MONGODB_URI; // Make sure this environment variable is set
@@ -27,91 +28,69 @@ mongoose.connect(mongoURI, {
   console.error('Failed to connect to MongoDB', err);
 });
 
-//Connect With Database
-MONGODB_URI = "mongodb+srv://admin:admin@cluster0.2okosc6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Middlewares
+app.use(express.static('public'));
+app.use(session({
+  secret: 'keyboardcat',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use(express.json());
+app.use(express.urlencoded());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+// Global middleware to set user in locals
+app.use('*', function(req, res, next) {
+  res.locals.user = req.session.user;
+  next();
 });
 
-//Controller Files
-const GController = require('./controllers/g')
-const G2Controller = require('./controllers/g2')
-const G2UpdateUserController = require('./controllers/G2UpdateUser')
-const GetDataController = require('./controllers/getData')
-const UpdateDataController = require('./controllers/updateData')
-const SignUpController = require('./controllers/SignUp')
-const NewSignUpController = require('./controllers/NewSignUp.js')
-const LoginController = require('./controllers/login')
-const UserLoginController = require('./controllers/UserLogin')
-const LogoutController = require('./controllers/logout')
-const AppointmentController = require('./controllers/Appointment.js')
-const CreateAppointmentController = require('./controllers/CreateAppointment.js')
-const AvailAppointmentController= require('./controllers/AvailAppointment');
-const BookAppoinmentController= require('./controllers/BookAppointment');
-const GetAppointmentController= require('./controllers/GetAppointment');
-const UpdateAppointmentController= require('./controllers/UpdateAppointment');
+// Controller Files
+const GController = require('./controllers/g');
+const G2Controller = require('./controllers/g2');
+const G2UpdateUserController = require('./controllers/G2UpdateUser');
+const GetDataController = require('./controllers/getData');
+const UpdateDataController = require('./controllers/updateData');
+const SignUpController = require('./controllers/SignUp');
+const NewSignUpController = require('./controllers/NewSignUp.js');
+const LoginController = require('./controllers/login');
+const UserLoginController = require('./controllers/UserLogin');
+const LogoutController = require('./controllers/logout');
+const AppointmentController = require('./controllers/Appointment.js');
+const CreateAppointmentController = require('./controllers/CreateAppointment.js');
+const AvailAppointmentController = require('./controllers/AvailAppointment');
+const BookAppoinmentController = require('./controllers/BookAppointment');
+const GetAppointmentController = require('./controllers/GetAppointment');
+const UpdateAppointmentController = require('./controllers/UpdateAppointment');
 
-const { update } = require('./models/User');
-
-//MiddleWares
+// Middlewares for routes
 const authMiddleware = require('./middleware/authMiddleware');
 const defaultCheckerMiddleware = require('./middleware/defaultchecker');
 const AdminAuthMiddleware = require('./middleware/AdminAuthMiddleware');
 const ExaminerAuthMiddleware = require('./middleware/ExaminerAuthMiddleware');
 
-app.use(express.static('public'))
-app.set('view engine','ejs')
-app.use(session({
-  secret: 'keyboardcat',
-  resave: false,
-  saveUninitialized: true
-}))
-app.use(flash());
-app.use(express.json())
-app.use(express.urlencoded())
-
-app.use('*',function(req, res, next) 
-{
-  res.locals.user = req.session.user;
-  next();
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.listen(4000, ()=>{
-    console.log('App listening on port 4000')
-})
+app.get('/g2_test', [authMiddleware, defaultCheckerMiddleware], BookAppoinmentController);
+app.get('/GetAppointment', AvailAppointmentController);
+app.get('/events/:id', GetAppointmentController);
+app.get('/g_test', [authMiddleware, defaultCheckerMiddleware], GController);
+app.get('/login', LoginController);
+app.post('/user/login', UserLoginController);
+app.get('/logout', LogoutController);
+app.get('/signup', SignUpController);
+app.post("/update/user", G2UpdateUserController);
+app.post('/update/Appointment', UpdateAppointmentController);
+app.post("/GetData", GetDataController);
+app.post("/UpdateData", UpdateDataController);
+app.post("/user/signup", NewSignUpController);
+app.get("/appointment", AdminAuthMiddleware, AppointmentController);
+app.post("/create/appointment", CreateAppointmentController);
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
-
-app.get('/g2_test',[authMiddleware,defaultCheckerMiddleware],BookAppoinmentController);
-
-app.get('/GetAppointment',AvailAppointmentController);
-
-app.get('/events/:id',GetAppointmentController);
-
-app.get('/g_test',[authMiddleware,defaultCheckerMiddleware],GController);
-
-app.get('/login',LoginController);
-
-app.post('/user/login',UserLoginController);
-
-app.get('/logout',LogoutController);
-
-app.get('/signup',SignUpController);
-
-app.post("/update/user",G2UpdateUserController);
-
-app.post('/update/Appointment',UpdateAppointmentController);
-
-app.post("/GetData",GetDataController);
-
-app.post("/UpdateData",UpdateDataController);
-
-app.post("/user/signup",NewSignUpController);
-
-app.get("/appointment",AdminAuthMiddleware,AppointmentController);
-
-app.post("/create/appointment",CreateAppointmentController);
+// Start server
+app.listen(4000, () => {
+  console.log('App listening on port 4000');
+});
